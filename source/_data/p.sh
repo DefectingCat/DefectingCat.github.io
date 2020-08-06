ca0e1d61c319db97932ab52a6fca66335dd943f0
DefectingCat <i@defect.ink>
2020-08-06 11:12:54 +0800

    👌All done

---
+++ source/_data/p.sh
@@ -0,0 +1,35 @@
+echo -e "Do someting："
+echo -e "\033[31m 1.Bug \033[0m \033[36m 2.Update \033[0m \033[35m 3.Docs \033[0m \033[29m 4.Heart \033[0m"
+read git
+#read input
+case $git in
+    1)  echo -n 'Bug：'
+        read input
+        git add -A
+        git commit -m "🐛$input"
+        git push
+    ;;
+    2)  echo -n 'Update：'
+        read input
+        git add -A
+        git commit -m "🛠$input"
+        git push
+    ;;
+    3)  echo -n 'Docs：'
+        read input
+        git add -A
+        git commit -m "📃$input"
+        git push
+    ;;
+    4)  echo -n 'Heart：'
+        read input
+        git add -A
+        git commit -m "❤$input"
+        git push
+    ;;
+    *)  echo 'Maybe not today.'
+    ;;
+esac
+# git add -A
+# git commit -m "❤$input"
+# git push
