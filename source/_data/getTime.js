0c5f25181fab8d8f8b52f57c768b79f512a963aa
DefectingCat <i@defect.ink>
2020-10-26 11:54:24 +0800

    🎁更新文章

---
+++ source/_data/getTime.js
@@ -0,0 +1,5 @@
+let d = new Date();
+
+let result = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':'  + d.getSeconds();
+
+console.log(result);
