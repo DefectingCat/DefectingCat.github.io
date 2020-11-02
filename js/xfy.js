8c7085f18f2802c934660c95c9c48539ada97f7b
DefectingCat <i@defect.ink>
2020-11-02 02:17:54 +0000

    ❤

---
+++ js/xfy.js
@@ -0,0 +1,1 @@
+$(document).ready((function(){$("#comments").on("focus",".v[data-class=v] .vwrap .vedit",(function(){$(this).animate({"background-size":"0%"})})),$("#comments").on("blur",".v[data-class=v] .vwrap .vedit",(function(){$(this).animate({"background-size":"20%"})}))}));
