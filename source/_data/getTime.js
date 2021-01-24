e4c3f1b55971f556f453ed67f61fc30e6fef840c
DefectingCat <i@defect.ink>
2021-01-24 10:54:15 +0800

    test:delete

--- source/_data/getTime.js
+++
@@ -1,5 +0,0 @@
-let d = new Date();
-
-let result = d.getFullYear() + '-' + (d.getMonth() + 1) + '-' + d.getDate() + ' ' + d.getHours() + ':' + d.getMinutes() + ':'  + d.getSeconds();
-
-console.log(result);
