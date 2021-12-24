971962069e8bf3c28791c5f39d7ec5802e76bee9
Defectink <i@defect.ink>
2021-12-24 10:46:40 +0800

    Add mdx

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
+
+const withMDX = require('@next/mdx')({
+  extension: /\.mdx?$/,
+  options: {
+    remarkPlugins: [],
+    rehypePlugins: [],
+  },
+});
+
+module.exports = withMDX({
   reactStrictMode: true,
-};
+  pageExtensions: ['js', 'jsx', 'md', 'mdx'],
+});
