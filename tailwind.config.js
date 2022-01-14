7d1ed6d80d4aed1b0eae0358134c7e38556ac2c0
DefectingCat <i@defect.ink>
2022-01-14 10:17:58 +0800

    Add tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+    './layouts/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        bluish: {
+          gray: 'rgba(245,247,250)',
+        },
+      },
+    },
+  },
+  plugins: [],
+};
