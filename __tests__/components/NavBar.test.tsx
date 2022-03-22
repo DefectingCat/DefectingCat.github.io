c651389aa30724961ac1652080c1932c6a85c1fe
Defectink <i@defect.ink>
2022-03-22 15:10:46 +0800

    Add jest

---
+++ __tests__/components/NavBar.test.tsx
@@ -0,0 +1,27 @@
+import { render, screen } from '@testing-library/react';
+import NavBar from 'components/NavBar';
+import '@testing-library/jest-dom';
+
+describe('NavBar', () => {
+  it('renders a title', async () => {
+    render(<NavBar />);
+
+    const heading = await screen.findByText(/RUA!/i);
+
+    expect(heading).toBeInTheDocument();
+  });
+  it('renders menu items', async () => {
+    render(<NavBar />);
+
+    const home = await screen.findByText(/Home/i);
+    const blog = await screen.findByText(/Blog/i);
+
+    expect(home).toBeInTheDocument();
+    expect(blog).toBeInTheDocument();
+
+    expect(home.hasAttribute('href')).toBeTruthy();
+    expect(home.getAttribute('href')).toEqual('/');
+    expect(blog.hasAttribute('href')).toBeTruthy();
+    expect(blog.getAttribute('href')).toEqual('/blog');
+  });
+});
