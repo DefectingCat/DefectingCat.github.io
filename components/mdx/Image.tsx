5cec1c504ff08a2f8bbb2d94875db1e60ba431fc
Defectink <i@defect.ink>
2022-04-06 15:12:01 +0800

    Add mdx image component * update markdown styles

---
+++ components/mdx/Image.tsx
@@ -0,0 +1,26 @@
+import cn from 'classnames';
+import NextImage from 'next/image';
+import { DetailedHTMLProps, ImgHTMLAttributes } from 'react';
+
+interface Props
+  extends DetailedHTMLProps<
+    ImgHTMLAttributes<HTMLImageElement>,
+    HTMLImageElement
+  > {}
+
+const Image = ({ src, alt }: Props) => {
+  return (
+    <>
+      <NextImage
+        src={src ?? ''}
+        alt={alt}
+        layout="responsive"
+        width="100%"
+        height="100%"
+      />
+      <span className="block text-center text-gray-400">{alt}</span>
+    </>
+  );
+};
+
+export default Image;
