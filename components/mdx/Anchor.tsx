c839e6dd57c2618e4447a616c76fc572c5657c88
Defectink <i@defect.ink>
2022-04-20 10:13:11 +0800

    Update image url
    Fix anchor style

--- components/mdx/Anchor.tsx
+++ components/mdx/Anchor.tsx
@@ -11,17 +11,21 @@ const Anchor = ({ children, external = true, ...rest }: Props) => {
     <>
       <a
         {...rest}
+        className="inline-block"
         target={external ? '_blank' : undefined}
-        className={cn(
-          'mx-[2px] text-teal-500 relative',
-          'before:left-0 before:top-[1px] before:block before:absolute',
-          'before:w-full before:h-full before:transition-all before:shadow-underline',
-          'hover:before:shadow-throughline',
-          'dark:text-teal-600'
-        )}
       >
-        {children}
-        {external && <FiExternalLink className="inline ml-1 mb-[0.2rem]" />}
+        <span
+          className={cn(
+            'mx-[2px] text-teal-500 relative',
+            'before:left-0 before:top-[1px] before:block before:absolute',
+            'before:w-full before:h-full before:transition-all before:shadow-underline',
+            'hover:before:shadow-throughline',
+            'dark:text-teal-600'
+          )}
+        >
+          {children}
+          {external && <FiExternalLink className="inline ml-1 mb-[0.2rem]" />}
+        </span>
       </a>
     </>
   );
