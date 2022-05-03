50a190ed6492ea9933c9565267ce3b57c3e27e55
DefectingCat <i@defect.ink>
2022-05-03 12:37:09 +0800

    Add post toc animation

--- components/post/PostTOC.tsx
+++ components/post/PostTOC.tsx
@@ -1,24 +1,60 @@
 import { getHeadings } from 'lib/utils';
 import Anchor from 'components/mdx/Anchor';
+import styles from './PostTOC.module.css';
+import classNames from 'classnames';
+import { useCallback, useState } from 'react';
+import { FiChevronDown } from 'react-icons/fi';
 
 interface Props {
   headings: ReturnType<typeof getHeadings>;
 }
 
 const PostTOC = ({ headings }: Props) => {
+  const [show, setShow] = useState(false);
+  const handleClick = useCallback(() => setShow((show) => !show), []);
+
   return (
     <>
-      <h2>What&apos;s inside?</h2>
+      <div
+        className={classNames(
+          'rounded-lg transition-all',
+          'duration-500 overflow-hidden'
+        )}
+        style={{
+          maxHeight: show ? (headings?.length ?? 0) * 50 + 70 : 70,
+        }}
+      >
+        <h2
+          className={classNames(
+            styles.head,
+            'bg-white !m-[unset] p-4',
+            'rounded-lg border border-gray-300',
+            'dark:bg-rua-gray-800 dark:border-rua-gray-600',
+            'select-none cursor-pointer',
+            'flex justify-between items-center'
+          )}
+          onClick={handleClick}
+        >
+          <span>What&apos;s inside?</span>
 
-      <ul className="pl-4 border-l-4 border-gray-300 toc">
-        {headings?.map((h) => (
-          <li key={h.link}>
-            <Anchor href={h.link} external={false}>
-              {h.text}
-            </Anchor>
-          </li>
-        ))}
-      </ul>
+          <FiChevronDown
+            className={classNames(
+              show && 'rotate-180',
+              'transition-all duration-500'
+            )}
+          />
+        </h2>
+
+        <ul className="pl-4 border-l-4 border-gray-300 toc">
+          {headings?.map((h) => (
+            <li key={h.link}>
+              <Anchor href={h.link} external={false}>
+                {h.text}
+              </Anchor>
+            </li>
+          ))}
+        </ul>
+      </div>
     </>
   );
 };
