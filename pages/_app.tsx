43df816bba7862a4ccdb02b21139ac4a678fe70b
Defectink <i@defect.ink>
2022-05-11 09:56:20 +0800

    Add router loading

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -7,6 +7,13 @@ import 'styles/prism-one-dark.css';
 import 'styles/rua.css';
 import { MDXProvider } from '@mdx-js/react';
 import Anchor from 'components/mdx/Anchor';
+import { useRouter } from 'next/router';
+import { useCallback, useState, useEffect } from 'react';
+import dynamic from 'next/dynamic';
+
+const VercelLoading = dynamic(
+  () => import('components/RUA/loading/VercelLoading')
+);
 
 const components = {
   a: Anchor,
@@ -14,6 +21,35 @@ const components = {
 
 function MyApp({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout ?? ((page) => page);
+
+  const router = useRouter();
+  const [loading, setLoading] = useState(false);
+
+  const handleStart = useCallback(
+    (url: string) => {
+      url !== router.pathname ? setLoading(true) : setLoading(false);
+    },
+    [router.pathname]
+  );
+
+  const handleComplete = useCallback(() => {
+    setLoading(false);
+  }, []);
+
+  useEffect(() => {
+    router.events.on('routeChangeStart', handleStart);
+    router.events.on('routeChangeComplete', handleComplete);
+    router.events.on('routeChangeError', handleComplete);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method:
+    return () => {
+      router.events.off('routeChangeStart', handleStart);
+      router.events.off('routeChangeComplete', handleComplete);
+      router.events.off('routeChangeError', handleComplete);
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <>
@@ -36,6 +72,8 @@ function MyApp({ Component, pageProps }: AppPropsWithLayout) {
           {getLayout(<Component {...pageProps} />)}
         </MDXProvider>
       </ThemeProvider>
+
+      {loading && <VercelLoading />}
     </>
   );
 }
