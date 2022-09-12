7a6acd0a90c02113ab919b0f3adbd8d31e3cf7fc
DefectingCat <i@defect.ink>
2022-09-12 11:32:54 +0800

    fix syntax

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,7 +20,7 @@ const Home: NextPageWithLayout = () => {
         <div className="z-0 w-full max-w-3xl px-4 my-4 text-2xl">
           <div className="max-w-xl leading-10">
             <h1 className="pb-4 text-4xl">Hi there 👋, I&apos;m Arthur. </h1>
-            <p>I&apos;m a Front-end developer. Yes, that&apos;s mean</p>
+            <p>I&apos;m a Front-end developer. Yes, that means</p>
             <p
               onMouseOver={() => setShowLang(true)}
               onMouseLeave={() => setShowLang(false)}
