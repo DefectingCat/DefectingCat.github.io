642b8be2ef06e412f8e1aedb32fe4caa07231955
DefectingCat <i@defect.ink>
2022-09-30 17:07:36 +0800

    add remove console

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,3 @@
-// import composePlugins from 'next-compose-plugins';
-
-// const composedConfig = composePlugins([
-//   /** @type {import('next').NextConfig} */
-//   {
-//     reactStrictMode: true,
-//     output: 'standalone',
-//     experimental: {
-//       // runtime: 'nodejs',
-//       // outputStandalone: true,
-//       images: { allowFutureImage: true },
-//     },
-//     pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
-//     images: { domains: ['avatars.githubusercontent.com'] },
-//   },
-// ]);
-
 /**
  * @type {import('next').NextConfig}
  */
@@ -28,6 +11,9 @@ const nextConfig = {
   experimental: {
     // runtime: 'nodejs',
     // outputStandalone: true,
+  },
+  compiler: {
+    removeConsole: true,
   },
   // assetPrefix: isExport ? './' : undefined,
   // images:
