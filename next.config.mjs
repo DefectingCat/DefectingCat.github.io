3a03411e93138e04db5f3c83688dff94f623aa12
DefectingCat <i@defect.ink>
2023-03-20 17:30:22 +0800

    Remove useless commnet

--- next.config.mjs
+++ next.config.mjs
@@ -18,13 +18,6 @@ const nextConfig = {
     removeConsole: process.env.NODE_ENV === 'production',
   },
   // assetPrefix: isExport ? './' : undefined,
-  // images:
-  //   process.env.NEXT_BUILD === 'export'
-  //     ? {
-  //         loader: 'imgix',
-  //         path: 'https://rua.plus/',
-  //       }
-  //     : {},
   pageExtensions: ['ts', 'tsx', 'js', 'jsx', 'md', 'mdx'],
 };
 
