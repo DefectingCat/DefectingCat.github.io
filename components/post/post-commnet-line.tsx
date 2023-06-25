b02d3d98e8d37dc2bfdc1f886993b99761bb24bf
DefectingCat <i@defect.ink>
2023-06-25 14:59:22 +0800

    fix hydration error

--- components/post/post-commnet-line.tsx
+++ components/post/post-commnet-line.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import useMounted from 'lib/hooks/use-mounted';
 import { useTheme } from 'next-themes';
 import Image from 'next/image';
 import { memo } from 'react';
@@ -8,16 +9,27 @@ const PostCommnetLine = () => {
   const { systemTheme, theme } = useTheme();
   const currentTheme = theme === 'system' ? systemTheme : theme;
 
+  const { mounted } = useMounted();
+
   return (
     <>
       <div className="flex justify-center select-none">
-        {currentTheme === 'dark' ? (
-          <Image
-            src="/images/img/comment-line-dark.svg"
-            width={300}
-            height={150}
-            alt=""
-          />
+        {mounted ? (
+          currentTheme === 'dark' ? (
+            <Image
+              src="/images/img/comment-line-dark.svg"
+              width={300}
+              height={150}
+              alt=""
+            />
+          ) : (
+            <Image
+              src="/images/img/comment-line.svg"
+              width={300}
+              height={150}
+              alt=""
+            />
+          )
         ) : (
           <Image
             src="/images/img/comment-line.svg"
