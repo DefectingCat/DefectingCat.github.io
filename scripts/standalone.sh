44940a79785d21e26d4cbba56adc87cbf978067b
xfy <i@rua.plus>
2024-03-27 13:56:58 +0800

    fix(build): tar folders structure

--- scripts/standalone.sh
+++ scripts/standalone.sh
@@ -1,5 +1,7 @@
 pnpm build-local
 mkdir .next/standalone/public && cp -aR public/* .next/standalone/public
 mkdir .next/standalone/public/_next && cp -aR .next/static/ .next/standalone/public/_next
-tar zcvf .next/standalone.tar.gz .next/standalone/
+cd .next/
+tar zcvf standalone.tar.gz standalone/
+cd ..
 ls -l .next/
