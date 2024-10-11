ed364647179622653902927d224c20763088db64
xfy <xfyxfy@proton.me>
2024-10-11 17:20:22 +0800

    chore(dev): switch to bun

--- scripts/standalone.sh
+++ scripts/standalone.sh
@@ -2,10 +2,10 @@
 
 set -xe
 
-pnpm build-local
+bun run build-local
 mkdir .next/standalone/public && cp -aR public/* .next/standalone/public
 mkdir .next/standalone/public/_next && cp -aR .next/static/ .next/standalone/public/_next
 cd .next/
 tar zcvf standalone.tar.gz standalone/
 cd ..
-ls -l .next/
+ls -lh .next/
